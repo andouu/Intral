8c032cd0439a05af7b1d895c030de8b28ed23d50
ashoka-man <63413643+ashoka-man@users.noreply.github.com>
2021-01-27 07:35:21 -0800

    Grade UI (Sort of)

--- react_native/App.js
+++ react_native/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {
+    useState
+} from 'react';
 import {
     SafeAreaView,
     StyleSheet,
@@ -6,6 +8,7 @@ import {
     View,
     Text,
     StatusBar,
+    TouchableHighlight
 } from 'react-native';
 
 import {
@@ -16,14 +19,66 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
     return (
         <>
-        <View style = {{flex: 1, backgroundColor: "powderblue", justifyContent: "center", alignItems: "center"}}>
-            <Text style = {{color: "red"}}>your grades</Text>
+        <View style = {styles.container}>
+            <Text style = {{fontSize: 30, fontWeight: "bold"}}>Grades</Text>
+            <View style = {{flexDirection: "column", alignItems: "center", paddingTop: 15}}>
+                <View style = {styles.grade_display}>
+                    <Text style = {styles.grade_letter}>A</Text>
+                    <View style = {styles.vertical_line}></View>
+                    <Text style = {styles.grade_info}>Your class info, percentage, blah blah</Text>
+                </View>
+                <View style = {styles.grade_display}>
+                    <Text style = {styles.grade_letter}>A</Text>
+                    <View style = {styles.vertical_line}></View>
+                    <Text style = {styles.grade_info}>Your class info, percentage, blah blah</Text>
+                </View>
+                <View style = {styles.grade_display}>
+                    <Text style = {styles.grade_letter}>A</Text>
+                    <View style = {styles.vertical_line}></View>
+                    <Text style = {styles.grade_info}>Your class info, percentage, blah blah</Text>
+                </View>
+                <View style = {styles.grade_display}>
+                    <Text style = {styles.grade_letter}>A</Text>
+                    <View style = {styles.vertical_line}></View>
+                    <Text style = {styles.grade_info}>Your class info, percentage, blah blah</Text>
+                </View>
+            </View>
         </View>
         </>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 35,
+        flex: 1,
+    },
+    grade_display: {
+        backgroundColor: "#DDDDDD",
+        height: "16%",
+        width: "100%",
+        flexDirection: "row",
+        marginTop: "5%",
+        marginBottom: "5%",
+        justifyContent: "center",
+        padding: "5%",
+    },
+    grade_letter: {
+        marginRight: "5%",
+        fontSize: 40,
+        fontWeight: "bold"
+    },
+    vertical_line:{
+        height: "100%",
+        width: 2,
+        backgroundColor: "#909090",
+    },
+    grade_info: {
+        marginLeft: "5%"
+    }
+});
 
 export default App;
