ab180d8b59ba1c5e1526610189366c40f3c1f01e
Andrew Zhou <andrewzhou16@gmail.com>
2021-08-08 14:25:57 -0700

    Add: Populated calendar based on date

--- react_native/screens/CalendarScreen.js
+++ react_native/screens/CalendarScreen.js
@@ -17,15 +17,104 @@ import Animated, {
 } from 'react-native-reanimated';
 
 const monthDict = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const SingleCalendar = ({ size, hwr=75, view='single', style }) => { // hwr = height width ratio
-    let height = size * 0.75;
+function dayOfWeek(d, m, y) { // https://www.geeksforgeeks.org/find-day-of-the-week-for-a-given-date/
+    let t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ];
+    y -= (m < 3) ? 1 : 0;
+    return ( y + y/4 - y/100 + y/400 + t[m-1] + d) % 7;
+}
+
+const CalendarDay = ({ displayDay, month, year, theme }) => {
+    const dateToday = new Date();
+    const dayToday = dateToday.getDate();
+    const monthToday = dateToday.getMonth();
+    const yearToday = dateToday.getFullYear();
+
+    const sameDay = (displayDay === dayToday && monthToday === month && yearToday === year);
+
+    return (
+        <View style={[styles.calendar_day, {}]}>
+            <View style={[styles.calendar_day_selected, {backgroundColor: sameDay ? theme.s6 : 'transparent'}]}>
+                <Text style={{fontFamily: 'ProximaNova-Regular', right: 0.4, color: sameDay ? theme.s1 : theme.s4}}>
+                    {displayDay}
+                </Text>
+            </View>
+        </View>
+    );
+}
+
+const SingleCalendar = ({ size, hwr=75, view='single', style, theme, month, year }) => { // hwr = height width ratio
+    let height = size * hwr/100;
     if(typeof size === 'string') {
         height = parseInt(size) * (hwr/100) + '%';
     }
+    const getDaysOfMonth = (month) => { // days in month
+        if(month === 2) {
+            if(year % 400 === 0)
+                return 29;
+            else if(year % 100 === 0)
+                return 28;
+            else if(year % 4 === 0)
+                return 29;
+            else
+                return 28;
+        } else if(month === 4 || month === 6 || month === 9 || month === 11) {
+            return 30;
+        } else {
+            return 31;
+        }
+    }
+    const numDays = getDaysOfMonth(month+1);
+
+    let dayLabels = daysOfWeek.map((day, index) => {
+        return (
+            <View 
+                key={index} 
+                style={{
+                    flex: 1, 
+                    alignItems: 'center', 
+                    justifyContent: 'center', 
+                    borderBottomWidth: StyleSheet.hairlineWidth, 
+                    borderBottomColor: theme.s4,
+                }}
+            >
+                <Text style={{fontFamily: 'Proxima Nova Bold', fontSize: 12, color: theme.s4}}>{day.substr(0, 2)}</Text>
+            </View>
+        );
+    });
+
+    let dayBoxes = [];
+    let count = '';
+    let firstDayOfMonth = Math.round(dayOfWeek(1, month+1, year))%7;
+    let startedCounting = false;
+    let finishedCounting = false;
+    for(let i=0; i<42; i++) {
+        if(i%7 === firstDayOfMonth && !startedCounting && !finishedCounting) {
+            startedCounting = true;
+            count = 0;
+        }
+        if(startedCounting) {
+            count++;
+            if(count > numDays) {
+                count = '';
+                startedCounting = false;
+                finishedCounting = true;
+            }
+        }
+        dayBoxes.push(
+            <CalendarDay key={i} theme={theme} displayDay={count} month={month} year={year} />
+        );
+    }
+
     return (
-        <Animated.View style={[{width: size, height: height, backgroundColor: 'red'}, style]}>
-            <Text>Calendar here lol</Text>
+        <Animated.View style={[{width: size, height: height}, style]}>
+            <View style={{flex: 2, flexDirection: 'row', marginBottom: 10}}>
+                {dayLabels}
+            </View>
+            <View style={{flex: 10, flexDirection: 'row', flexWrap: 'wrap'}}>
+                {dayBoxes}
+            </View>
         </Animated.View>
     );
 }
@@ -61,7 +150,7 @@ const Calendar = ({ themeData }) => {
         setSelectedDate({
             month: dateToday.getMonth(),
             year: dateToday.getFullYear(),
-        })
+        });
     }, [isFocused])
 
     return (
@@ -110,7 +199,7 @@ const Calendar = ({ themeData }) => {
                     <MaterialDesignIcons name='chevron-right' size={35} color={theme.s4} style={{left: 2}} />
                 </Pressable>
             </View>
-            <SingleCalendar size={'100%'} hwr={80} />
+            <SingleCalendar size={'100%'} hwr={80} theme={theme} month={selectedDate.month} year={selectedDate.year} />
         </View>
     );
 }
@@ -202,7 +291,7 @@ const styles = StyleSheet.create({
     },
     calendar_container: {
         width: '100%', 
-        height: 280,
+        height: 300,
     },
     calendar_header: {
         width: '100%',
@@ -212,6 +301,19 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         marginBottom: 10,
     },
+    calendar_day: {
+        width: 100/7+'%', 
+        height: 100/6+'%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    calendar_day_selected: {
+        width: 25, 
+        height: 25, 
+        borderRadius: 30, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+    }
 });
 
 export default CalendarScreen;
